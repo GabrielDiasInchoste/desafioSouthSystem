58c9d2f885e1223db237f1cdff5f72411f7d4225
Gabriel Dias <gabriel.dias97@hotmail.com>
2020-08-15 18:37:54 -0300

    GD-01 Criacao do projeto

---
+++ src/test/java/br/com/gabrielDias/desafioSouthSystem/DesafioSouthSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gabrielDias.desafioSouthSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioSouthSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
